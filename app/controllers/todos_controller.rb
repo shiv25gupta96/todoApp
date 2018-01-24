92471aaa67e1b1881b91491cd30008a1d8028900
shiv25gupta96 <shivam25gupta96@gmail.com>
2018-01-25 01:20:04 +0530

    Refactored code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,14 +1,15 @@
 class TodosController < ApplicationController
+    before_action :set_todo, only: ['show', 'edit', 'update', 'destroy']
 
     def new
-        @any = Todo.new;
+        @todo = Todo.new;
     end
 
     def create
-        @any = Todo.new(require_permit)
-        if @any.save
+        @todo = Todo.new(require_permit)
+        if @todo.save
             flash[:notice] = "New Todo created successfully"
-            redirect_to todo_url(@any)
+            redirect_to todo_url(@todo)
         else
             render 'new'
         end
@@ -19,15 +20,12 @@ class TodosController < ApplicationController
     end
 
     def show
-        @todo = Todo.find(params[:id])
     end
 
     def edit
-        @todo = Todo.find(params[:id])
     end
 
     def update
-        @todo = Todo.find(params[:id])
         if @todo.update(require_permit)
             flash[:notice] = "Edited Successfully"
             redirect_to todo_path(@todo)
@@ -37,13 +35,16 @@ class TodosController < ApplicationController
     end
 
     def destroy
-        @todo = Todo.find(params[:id])
-        if @todo.delete
-            redirect_to todos_path
-        end
+        @todo.delete
+        flash[:notice] = "Deleted Successfully"
+        redirect_to todos_path
     end
 
     private
+
+        def set_todo
+            @todo = Todo.find(params[:id])
+        end
         def require_permit
             params.require(:todo).permit(:name, :description);
         end
